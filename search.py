9a64d2a9e126dd5913fa01a74362292b0021a185
Ben Taylor <bewt85@gmail.com>
2014-08-08 20:59:15 +0000

    Doing regex in memory now

--- search.py
+++ search.py
@@ -6,4 +6,7 @@ search_string = re.compile('bbb')
 matchingLineMap = lambda line: 1 if search_string.search(line) else 0
 
 with open(filename, 'r') as f:
-  matchingLineCount = sum(map(matchingLineMap, f))
+  text = f.read().split('\n')
+
+print "Starting search"
+matchingLineCount = sum(map(matchingLineMap, text))

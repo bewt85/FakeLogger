e9bc0600a99eca5f55982eb14d93ef227fd46f11
Ben Taylor <bewt85@gmail.com>
2014-08-09 08:59:24 +0000

    Creates pretty ok fake log data

--- createFakeLogData.py
+++ createFakeLogData.py
@@ -1,4 +1,4 @@
-import random, numpy
+import random, numpy, datetime
 
 # Format
 # ip_address userId dateTime "request" status page_size deviceFingerprint
@@ -23,9 +23,10 @@ class Customer(object):
     self.userId = getNextUserId()
     self.deviceId = getNextDeviceId()
 
-  def start(self, page, time):
+  def start(self, page, time=None):
     self.page = page
-    self.time = time
+    self.time = time if time else datetime.datetime.now()
+    self.history = []
 
   @classmethod
   def calculateCumulativeWeightedTransitions(cls, weighted_transitions):
@@ -44,13 +45,35 @@ class Customer(object):
  
   def step(self):
     if self.page.quitProb > random.random():
-      return None
+      self.page = None
+      return
     if not self.page.transitions:
-      return None
+      self.page = None
+      return
     transition = self.pick(self.page.transitions, random.random())
     self.page = transition.destination
-    self.time += numpy.random.poisson(transition.timeBeforePageChange)
+    self.time += datetime.timedelta(0,numpy.random.poisson(transition.timeBeforePageChange))
     self.history.append((self.time, transition))
+
+  def createHistory(self, length=0):
+    while self.page and (length > 0 or len(self.history <= length)):
+      self.step()
+
+  def formatHistory(self):
+    for (transitionTime, transition) in self.history:
+      if transition.destination == None:
+        return
+      logline_data = {
+        "ip": self.ip,
+        "user": self.userId,
+        "time": transitionTime.strftime("[%d/%b/%Y:%H:%M:%S +0000]"),
+        "method": transition.method,
+        "destination": transition.destination.path,
+        "status": transition.status,
+        "size": transition.destination.size,
+        "fingerprint": self.deviceId
+      }
+      yield (transitionTime, "{ip} {user} {time} \"{method} {destination}\" {status} {size} {fingerprint}".format(**logline_data))
 
   def __repr__(self):
     return "User {}: ip = {}; device = {}".format(self.userId, self.ip, self.deviceId)
@@ -58,22 +81,31 @@ class Customer(object):
 class Transition(object):
   def __init__(self, source, destination, method='GET', status=None, timeBeforePageChange=120.0):
     self.source = source
-    self.destinaton = destination
+    self.destination = destination
     self.method = method
     self.timeBeforePageChange=120
     defaultStatus = {'GET': 200, 'POST': 201, 'PUT': 200}
     self.status = status if status else defaultStatus.get(method, 200)
 
+  def __repr__(self):
+    return "Transition: ({}) -> ({}) (method: {} status: {} in {} seconds)".format(
+      self.source, self.destination, self.method, 
+      self.status, self.timeBeforePageChange
+    )
+
 class Page(object):
-  def __init__(self, url, size=None, domain=None, quitProb=0.001):
-    self.url = url
-    self.domain = "www.example.com"
-    self.size = random.randint(200, 200000)
+  def __init__(self, path, size=None, domain=None, quitProb=0.001):
+    self.path = path
+    self.domain = domain if domain else "www.example.com"
+    self.size = size if size else random.randint(200, 200000)
     self.quitProb = quitProb
     self.transitions = []
 
   def addNextPage(self, page, method='GET', status=None, timeBeforePageChange=120.0, likelyhoodWeight=1):
     self.transitions.append((likelyhoodWeight, Transition(self, page, method, status, timeBeforePageChange)))
+  
+  def __repr__(self):
+    return "Page: <{},{}>".format(self.domain, self.path)
 
 if __name__ == "__main__":
 
@@ -121,5 +153,21 @@ if __name__ == "__main__":
       self.assertEqual(self.customer.pick(transitions, 0.8), transitionBA)
       self.assertEqual(self.customer.pick(transitions, 1.0), transitionBA)
 
+  class HistoryTest(unittest.TestCode):
+    def setUp(self):
+      self.customer = Customer()
+      self.pageA = Page('/pageA', quitProb=0.0)
+      self.pageB = Page('/pageB', quitProb=0.0)
+    
+    def test_singleTransition(self):
+      self.pageA.addNextPage(self.pageB) 
+      self.pageB.addNextPage(None)
+      self.customer.start(pageA)
+      self.customer.createHistory(10) 
+      self.assertEqual(len(self.customer.history), 2)
+      self.assertequal(self.customer.history[0][1].source, self.pageA)
+      self.assertequal(self.customer.history[0][1].destination, self.pageB)
+      self.assertequal(self.customer.history[1][1].source, self.pageB)
+      self.assertequal(self.customer.history[1][1].destination, None)
 
   unittest.main()
